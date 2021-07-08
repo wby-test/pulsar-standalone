8ffe1a7d4650b1a53f36bf59b7e3cf2870566ec5
wangbaoyi <wangbaoyi@atomr.cn>
2021-07-08 21:07:13 +0800

    pulsar cient test

---
+++ consumer/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/apache/pulsar-client-go/pulsar"
+)
+
+func main() {
+	client, err := pulsar.NewClient(pulsar.ClientOptions{
+		URL: "pulsar://:4540",
+	})
+
+	defer client.Close()
+	if err != nil {
+		fmt.Print("new client failed")
+	}
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            "my-topic",
+		SubscriptionName: "my-sub",
+		Type:             pulsar.Shared,
+	})
+
+	defer consumer.Close()
+
+	for i := 1; i < 20; i++ {
+		msg, err := consumer.Receive(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Received message msgId: %#v -- content: '%s'\n",
+			msg.ID(), string(msg.Payload()))
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github/wby0701/pulsar-standalone
+
+go 1.16
+
+require github.com/apache/pulsar-client-go v0.5.0
